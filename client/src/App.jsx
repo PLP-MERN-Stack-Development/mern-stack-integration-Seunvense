cd364d1a38cb4e9f8eb6b30b0921abfa15988460
Seunvense <90153850+Seunvense@users.noreply.github.com>
2025-11-03 17:06:26 +0100

    Task 3 (Setup): Vite, Router, App.jsx, folders

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PostList from './pages/PostList.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import PostDetail from './pages/PostDetail.jsx';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <div className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<PostList />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
